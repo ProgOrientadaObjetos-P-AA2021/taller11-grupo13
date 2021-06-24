66159665985a84791bbd7d33694ae66235f6691b
Your Name <you@example.com>
2021-06-24 16:04:35 -0500

    Cambio

---
+++ taller11/src/taller11/menu.java
@@ -0,0 +1,65 @@
+package taller11;
+
+/**
+ *
+ * @author josep
+ */
+public class menu {
+    
+    
+    private String nombreCliente;
+    private String listadoMenu;
+    private double valorTotal;
+    private double subtotal;
+    private double Iva;
+
+    public menu(String nombreCliente, String listadoMenu, double valorTotal, double subtotal, double Iva) {
+        this.nombreCliente = nombreCliente;
+        this.listadoMenu = listadoMenu;
+        this.valorTotal = valorTotal;
+        this.subtotal = subtotal;
+        this.Iva = Iva;
+    }
+
+    public double getIva() {
+        return Iva;
+    }
+
+    public void setIva(double Iva) {
+        this.Iva = Iva;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getListadoMenu() {
+        return listadoMenu;
+    }
+
+    public void setListadoMenu(String listadoMenu) {
+        this.listadoMenu = listadoMenu;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+    
+}
---
+++ taller11/src/taller11/menuCarta.java
@@ -0,0 +1,18 @@
+package taller11;
+
+/**
+ *
+ * @author josep
+ */
+public class menuCarta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+                     
+    }
+    
+    
+}
